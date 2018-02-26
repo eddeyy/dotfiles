6529218b723a733217ddf796509e0aec800adf7c
Eduard Leonhardt <eduard.leonhardt@outlook.de>
2018-02-26 21:04:34 +0100

    nvim: correction

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -5,10 +5,10 @@ script_home="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo apt-get install neovim
 
-ln $params $script_home/init.vim $HOME/.config/nvim/init.vim
-
 # Plugin Manager
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+ln $params $script_home/init.vim $HOME/.config/nvim/init.vim
 
 nvim -c ":PlugInstall"
