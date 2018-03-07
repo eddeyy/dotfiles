ddef45574ba8d6592fa1f8cd844d6268fd1a0bbe
Eduard Leonhardt <eduard.leonhardt@outlook.de>
2018-03-07 13:53:32 +0100

    Fish Config created. Improved setup.sh.

--- fish/setup.sh
+++ fish/setup.sh
@@ -11,12 +11,12 @@ sudo apt-get install fish -y
 # Install fisherman (Plugin Manager for fish)
 if [ ! -f $HOME/.config/fish ]; then
     curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-    fish ./plungin_install.fish
 fi
 
 # Start Plugin Installer Script
 fish $script_home/plugin_install.fish
 
 # Symlinks
-ln $params $script_home/fish_user_key_bindings.fish $HOME/.config/fish/functions/fish_user_key_bindings
-ln $params $script_home/config.fish $HOME/.config/fish/config.fsh
+ln $params $script_home/config.fish $HOME/.config/fish/config.fish
+
+sed -i '/(date +%H:%M:%S)/ s/^/#/' $HOME/.config/fish/functions/fish_right_prompt.fish
